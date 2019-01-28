6d2e4c7c6d396f2cb818a97b466ccee3c4c85455
compartida1 <cuentacompartida496@gmail.com>
2019-01-28 22:38:45 +0100

    segunda modificacion en issue54

--- HolaMundo.java
+++ HolaMundo.java
@@ -3,6 +3,7 @@ public class HolaMundo {
     public static void main(String[] args) {
 		System.out.println("Hola mundo.");
 		System.out.println("Primera mod. en issue54");
+		System.out.println("Segunda mod. en issue54");
     }
     
 }
